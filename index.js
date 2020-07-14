c029923a7338e397306a4326300f4c2b138ebe53
Divyansh <divi.sunway@gmail.com>
2020-07-14 13:38:22 +0530

    port

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
+const port=process.env.PORT || 3000;
 
 app.use(express.static("static"));
 
@@ -14,6 +15,6 @@ io.on("connection", (socket) => {
   });
 });
 
-http.listen(3000, () => {
-  console.log("listening on *:3000");
+http.listen(port, () => {
+  console.log(`listening on *:${port}`);
 });
