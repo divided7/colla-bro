9c546052bc7641882a491acd6a8b947e44c00233
Divyansh <divi.sunway@gmail.com>
2020-07-14 13:28:32 +0530

    mm

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
 
 app.use(express.static("static"));
 
-app.listen(port, () =>
-  console.log(`App listening at http://localhost:${port}`)
-);
+io.on("connection", (socket) => {
+  console.log(`Connected :${socket.id}`);
+  socket.on("disconnect", () => console.log(`${socket.id} has disconnected`));
+  socket.on("mouse", (data) => socket.broadcast.emit("mouse", data));
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
+});
+
+http.listen(3000, () => {
+  console.log("listening on *:3000");
+});
---
+++ static/chat.js
@@ -0,0 +1,10 @@
+$(function () {
+  $('form').submit(function(e) {
+    e.preventDefault(); // prevents page reloading
+    socket.emit('chat message', $('#m').val());
+    $('#m').val('');
+  });
+  socket.on('chat message', function(msg){
+    $('#messages').append($('<li>').text(msg));
+  });
+});
